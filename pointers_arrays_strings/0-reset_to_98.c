60d82d309f73b5769f7fb81ad51e0ec8cda44638
Vaikdowells <dowellsvaik@gmail.com>
2025-02-26 17:20:00 +0100

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,5 +2,5 @@
 
 void reset_to_98(int *n)
 {
-	*n = 98;  /* Déréférencement du pointeur pour modifier la valeur de la variable pointée */
+	*n = 98;  /* Modification de la valeur la variable pointée */
 }
