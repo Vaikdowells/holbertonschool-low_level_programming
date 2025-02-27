a08260394a3a03bbd41e47ed935c55ae68b2b045
Vaikdowells <dowellsvaik@gmail.com>
2025-02-27 16:11:39 +0100

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,11 @@
  * reset_to_98 - Met à jour la valeur pointée par un pointeur à 98
  * @n: Pointeur vers un entier dont la valeur doit être modifiée
  *
- * Description: Cette fonction prend un pointeur vers un int et 
+ * Description: Cette fonction prend un pointeur vers un int et
  * met la valeur de l'entier pointé à 98.
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
 
